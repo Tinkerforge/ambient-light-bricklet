d2c5fb03c2faf2d09e21eb238329e5710c43a07a
Olaf Lüke <olaf@tinkerforge.com>
2011-08-24 14:28:39 +0200

    initial commit

---
+++ software/src/ambient-light.c
@@ -0,0 +1,216 @@
+/* ambient-light-bricklet
+ * Copyright (C) 2010-2011 Olaf Lüke <olaf@tinkerforge.com>
+ *
+ * ambient-light.c: Implementation of Ambient Light Bricklet messages
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "ambient-light.h"
+#include <adc/adc.h>
+
+#include "bricklib/bricklet/bricklet_communication.h"
+#include "bricklib/utility/util_definitions.h"
+#include "brickletlib/bricklet_entry.h"
+#include "brickletlib/bricklet_simple.h"
+#include "config.h"
+
+#define ILLUMINANCE_AVERAGE 50
+
+#define SIMPLE_UNIT_ILLUMINANCE 0
+#define SIMPLE_UNIT_ANALOG_VALUE 1
+
+const SimpleMessageProperty smp[] = {
+	{SIMPLE_UNIT_ILLUMINANCE, SIMPLE_TRANSFER_VALUE, SIMPLE_DIRECTION_GET}, // TYPE_GET_ILLUMINANCE
+	{SIMPLE_UNIT_ANALOG_VALUE, SIMPLE_TRANSFER_VALUE, SIMPLE_DIRECTION_GET}, // TYPE_GET_ANALOG_VALUE
+	{SIMPLE_UNIT_ILLUMINANCE, SIMPLE_TRANSFER_PERIOD, SIMPLE_DIRECTION_SET}, // TYPE_SET_ILLUMINANCE_CALLBACK_PERIOD
+	{SIMPLE_UNIT_ILLUMINANCE, SIMPLE_TRANSFER_PERIOD, SIMPLE_DIRECTION_GET}, // TYPE_GET_ILLUMINANCE_CALLBACK_PERIOD
+	{SIMPLE_UNIT_ANALOG_VALUE, SIMPLE_TRANSFER_PERIOD, SIMPLE_DIRECTION_SET}, // TYPE_SET_ANALOG_VALUE_CALLBACK_PERIOD
+	{SIMPLE_UNIT_ANALOG_VALUE, SIMPLE_TRANSFER_PERIOD, SIMPLE_DIRECTION_GET}, // TYPE_GET_ANALOG_VALUE_CALLBACK_PERIOD
+	{SIMPLE_UNIT_ILLUMINANCE, SIMPLE_TRANSFER_THRESHOLD, SIMPLE_DIRECTION_SET}, // TYPE_SET_ILLUMINANCE_CALLBACK_THRESHOLD
+	{SIMPLE_UNIT_ILLUMINANCE, SIMPLE_TRANSFER_THRESHOLD, SIMPLE_DIRECTION_GET}, // TYPE_GET_ILLUMINANCE_CALLBACK_THRESHOLD
+	{SIMPLE_UNIT_ANALOG_VALUE, SIMPLE_TRANSFER_THRESHOLD, SIMPLE_DIRECTION_SET}, // TYPE_SET_ANALOG_VALUE_CALLBACK_THRESHOLD
+	{SIMPLE_UNIT_ANALOG_VALUE, SIMPLE_TRANSFER_THRESHOLD, SIMPLE_DIRECTION_GET}, // TYPE_GET_ANALOG_VALUE_CALLBACK_THRESHOLD
+	{0, SIMPLE_TRANSFER_DEBOUNCE, SIMPLE_DIRECTION_SET}, // TYPE_SET_DEBOUNCE_PERIOD
+	{0, SIMPLE_TRANSFER_DEBOUNCE, SIMPLE_DIRECTION_GET}, // TYPE_GET_DEBOUNCE_PERIOD
+};
+
+const SimpleUnitProperty sup[] = {
+	{illuminance_from_analog_value, SIMPLE_SIGNEDNESS_INT, TYPE_ILLUMINANCE, TYPE_ILLUMINANCE_REACHED, SIMPLE_UNIT_ANALOG_VALUE}, // illuminance
+	{analog_value_from_mc, SIMPLE_SIGNEDNESS_UINT, TYPE_ANALOG_VALUE, TYPE_ANALOG_VALUE_REACHED, SIMPLE_UNIT_ANALOG_VALUE}, // analog value
+};
+
+
+void invocation(uint8_t com, uint8_t *data) {
+	simple_invocation(com, data);
+}
+
+void constructor(void) {
+	adc_channel_enable(BS->adc_channel);
+	BC->value_avg = 0;
+
+    PIN_RESISTOR_1.type = PIO_OUTPUT_0;
+    BA->PIO_Configure(&PIN_RESISTOR_1, 1);
+
+    PIN_RESISTOR_2.type = PIO_INPUT;
+    BA->PIO_Configure(&PIN_RESISTOR_2, 1);
+
+    PIN_RESISTOR_3.type = PIO_INPUT;
+    BA->PIO_Configure(&PIN_RESISTOR_3, 1);
+
+    BC->current_resistor = 1;
+    BC->new_resistor = 1;
+
+	simple_constructor();
+}
+
+void destructor(void) {
+	simple_destructor();
+
+    PIN_RESISTOR_1.type = PIO_INPUT;
+    BA->PIO_Configure(&PIN_RESISTOR_1, 1);
+
+    PIN_RESISTOR_2.type = PIO_INPUT;
+    BA->PIO_Configure(&PIN_RESISTOR_2, 1);
+
+    PIN_RESISTOR_3.type = PIO_INPUT;
+    BA->PIO_Configure(&PIN_RESISTOR_3, 1);
+
+	adc_channel_disable(BS->adc_channel);
+}
+
+void set_new_resistor(void) {
+	if(BC->new_resistor == BC->current_resistor) {
+		return;
+	}
+
+	switch(BC->new_resistor) {
+		case 0:
+			PIN_RESISTOR_2.type = PIO_OUTPUT_0;
+			BA->PIO_Configure(&PIN_RESISTOR_2, 1);
+
+			PIN_RESISTOR_3.type = PIO_OUTPUT_0;
+			BA->PIO_Configure(&PIN_RESISTOR_3, 1);
+
+			PIN_RESISTOR_1.type = PIO_OUTPUT_0;
+			BA->PIO_Configure(&PIN_RESISTOR_1, 1);
+
+			BC->current_resistor = 0;
+			break;
+
+		case 1:
+		    PIN_RESISTOR_2.type = PIO_INPUT;
+		    BA->PIO_Configure(&PIN_RESISTOR_2, 1);
+
+		    PIN_RESISTOR_3.type = PIO_INPUT;
+		    BA->PIO_Configure(&PIN_RESISTOR_3, 1);
+
+		    PIN_RESISTOR_1.type = PIO_OUTPUT_0;
+		    BA->PIO_Configure(&PIN_RESISTOR_1, 1);
+
+		    BC->current_resistor = 1;
+		    break;
+
+		case 2:
+		    PIN_RESISTOR_1.type = PIO_INPUT;
+		    BA->PIO_Configure(&PIN_RESISTOR_1, 1);
+
+		    PIN_RESISTOR_3.type = PIO_INPUT;
+		    BA->PIO_Configure(&PIN_RESISTOR_3, 1);
+
+		    PIN_RESISTOR_2.type = PIO_OUTPUT_0;
+		    BA->PIO_Configure(&PIN_RESISTOR_2, 1);
+
+		    BC->current_resistor = 2;
+		    break;
+
+		case 3:
+		    PIN_RESISTOR_1.type = PIO_INPUT;
+		    BA->PIO_Configure(&PIN_RESISTOR_1, 1);
+
+		    PIN_RESISTOR_2.type = PIO_INPUT;
+		    BA->PIO_Configure(&PIN_RESISTOR_2, 1);
+
+		    PIN_RESISTOR_3.type = PIO_OUTPUT_0;
+		    BA->PIO_Configure(&PIN_RESISTOR_3, 1);
+
+		    BC->current_resistor = 3;
+		    break;
+	}
+
+	BC->new_resistor_set = 10;
+}
+
+void update_resistor(uint16_t value) {
+	if(value > 3800) {
+		if(BC->current_resistor > 0) {
+			BC->new_resistor = BC->current_resistor -1;
+		}
+	} else if(BC->current_resistor == 0 && value < THRESHOLD_VALUE_0) {
+		BC->new_resistor = 1;
+	} else if(BC->current_resistor == 1 && value < THRESHOLD_VALUE_1) {
+		BC->new_resistor = 2;
+	} else if(BC->current_resistor == 2 && value < THRESHOLD_VALUE_2) {
+		BC->new_resistor = 3;
+	}
+}
+
+int32_t analog_value_from_mc(int32_t value) {
+	if(BC->new_resistor_set > 0) {
+		return BC->value[0];
+	}
+	uint16_t analog_value = BA->adc_channel_get_data(BS->adc_channel);
+	update_resistor(analog_value);
+	return analog_value;
+}
+
+int32_t illuminance_from_analog_value(int32_t value) {
+	if(BC->new_resistor_set > 0) {
+		BC->new_resistor_set--;
+		return BC->value[1];
+	}
+
+	int32_t illuminance = 0;
+	switch(BC->current_resistor) {
+		case 0:
+			illuminance = VALUE_TO_MA_0(value);
+			break;
+		case 1:
+			illuminance = VALUE_TO_MA_1(value);
+			break;
+		case 2:
+			illuminance = VALUE_TO_MA_2(value);
+			break;
+		case 3:
+			illuminance = VALUE_TO_MA_3(value);
+			break;
+	}
+
+	set_new_resistor();
+
+	BC->value_avg_sum += illuminance;
+
+	if(BC->tick % ILLUMINANCE_AVERAGE == 0) {
+		BC->value_avg = BC->value_avg_sum/ILLUMINANCE_AVERAGE;
+		BC->value_avg_sum = 0;
+	}
+
+	return BC->value_avg;
+}
+
+void tick(void) {
+	simple_tick();
+}
---
+++ software/src/config.h
@@ -0,0 +1,96 @@
+/* ambient-light-bricklet
+ * Copyright (C) 2010-2011 Olaf Lüke <olaf@tinkerforge.com>
+ *
+ * config.h: Ambient Light Bricklet specific configuration
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "bricklib/drivers/board/sam3s/SAM3S.h"
+
+#include "ambient-light.h"
+
+#define MAX_ADC_VALUE ((1  << 12) - 1)
+#define MAX_VOLTAGE 3300
+
+#define VALUE_RESISTOR_0 7354  // 1000/((1/10) + (1/47) + (1/68))
+#define VALUE_RESISTOR_1 10000
+#define VALUE_RESISTOR_2 47000
+#define VALUE_RESISTOR_3 68000
+
+#define DIVISOR_0 (VALUE_RESISTOR_0*MAX_ADC_VALUE/MAX_VOLTAGE)
+#define DIVISOR_1 (VALUE_RESISTOR_1*MAX_ADC_VALUE/MAX_VOLTAGE)
+#define DIVISOR_2 (VALUE_RESISTOR_2*MAX_ADC_VALUE/MAX_VOLTAGE)
+#define DIVISOR_3 (VALUE_RESISTOR_3*MAX_ADC_VALUE/MAX_VOLTAGE)
+
+#define MAX_UA_0 (2*1000*MAX_ADC_VALUE/DIVISOR_0)
+#define MAX_UA_1 (2*1000*MAX_ADC_VALUE/DIVISOR_1)
+#define MAX_UA_2 (2*1000*MAX_ADC_VALUE/DIVISOR_2)
+#define MAX_UA_3 (2*1000*MAX_ADC_VALUE/DIVISOR_3)
+
+#define THRESHOLD_VALUE_0 (MAX_UA_2*DIVISOR_0/(2*1150)) // 15% debounce
+#define THRESHOLD_VALUE_1 (MAX_UA_2*DIVISOR_1/(2*1150)) // 15% debounce
+#define THRESHOLD_VALUE_2 (MAX_UA_3*DIVISOR_2/(2*1150)) // 15% debounce
+
+#define VALUE_TO_MA_0(value) ((2*1000*10*(value))/DIVISOR_0)
+#define VALUE_TO_MA_1(value) ((2*1000*10*(value))/DIVISOR_1)
+#define VALUE_TO_MA_2(value) ((2*1000*10*(value))/DIVISOR_2)
+#define VALUE_TO_MA_3(value) ((2*1000*10*(value))/DIVISOR_3)
+
+#define PIN_RESISTOR_1 (BS->pin2_da) // 10k
+#define PIN_RESISTOR_2 (BS->pin3_pwm) // 47k
+#define PIN_RESISTOR_3 (BS->pin4_io) // 68k
+
+#define LOGGING_LEVEL LOGGING_DEBUG
+#define DEBUG_BRICKLET 0
+
+#define BRICKLET_HAS_SIMPLE_SENSOR
+#define INVOCATION_IN_BRICKLET_CODE
+#define NUM_SIMPLE_VALUES 2
+
+typedef struct {
+	int32_t value[NUM_SIMPLE_VALUES];
+	int32_t last_value[NUM_SIMPLE_VALUES];
+	int16_t value_avg;
+	int32_t value_avg_sum;
+
+	uint32_t signal_period[NUM_SIMPLE_VALUES];
+	uint32_t signal_period_counter[NUM_SIMPLE_VALUES];
+
+	uint32_t threshold_debounce;
+	uint32_t threshold_period_current[NUM_SIMPLE_VALUES];
+	int32_t  threshold_min[NUM_SIMPLE_VALUES];
+	int32_t  threshold_max[NUM_SIMPLE_VALUES];
+	char     threshold_option[NUM_SIMPLE_VALUES];
+
+	int32_t  threshold_min_save[NUM_SIMPLE_VALUES];
+	int32_t  threshold_max_save[NUM_SIMPLE_VALUES];
+	char     threshold_option_save[NUM_SIMPLE_VALUES];
+
+	uint8_t current_resistor;
+	uint8_t new_resistor;
+	uint8_t new_resistor_set;
+
+	uint32_t tick;
+} BrickContext;
+
+#endif
