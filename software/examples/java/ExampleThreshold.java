7920bf022f467afe5f200c020a4f0be29bc68614
Matthias Bolte <matthias@tinkerforge.com>
2014-04-02 17:15:07 +0200

    java: Unify constants style

--- software/examples/java/ExampleThreshold.java
+++ software/examples/java/ExampleThreshold.java
@@ -2,17 +2,17 @@ import com.tinkerforge.BrickletAmbientLight;
 import com.tinkerforge.IPConnection;
 
 public class ExampleThreshold {
-	private static final String host = "localhost";
-	private static final int port = 4223;
+	private static final String HOST = "localhost";
+	private static final int PORT = 4223;
 	private static final String UID = "ABC"; // Change to your UID
-	
+
 	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
 	//       might normally want to catch are described in the documentation
 	public static void main(String args[]) throws Exception {
 		IPConnection ipcon = new IPConnection(); // Create IP connection
 		BrickletAmbientLight al = new BrickletAmbientLight(UID, ipcon); // Create device object
 
-		ipcon.connect(host, port); // Connect to brickd
+		ipcon.connect(HOST, PORT); // Connect to brickd
 		// Don't use device before ipcon is connected
 
 		// Get threshold callbacks with a debounce time of 10 seconds (10000ms)
