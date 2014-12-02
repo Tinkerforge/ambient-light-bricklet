78323bfcd4b2a7563d26cce025aff41b67900f1c
Olaf Lüke <olaf@tinkerforge.com>
2014-12-02 16:55:30 +0100

    Use ReadLine instead of ReadKey for C# example

--- software/examples/csharp/ExampleSimple.cs
+++ software/examples/csharp/ExampleSimple.cs
@@ -19,8 +19,8 @@ class Example
 
 		System.Console.WriteLine("Illuminance: " + illuminance/10.0 + " Lux");
 
-		System.Console.WriteLine("Press key to exit");
-		System.Console.ReadKey();
+		System.Console.WriteLine("Press enter to exit");
+		System.Console.ReadLine();
 		ipcon.Disconnect();
 	}
 }
--- software/examples/csharp/ExampleThreshold.cs
+++ software/examples/csharp/ExampleThreshold.cs
@@ -30,8 +30,8 @@ class Example
 		// Configure threshold for "greater than 200 Lux" (unit is Lux/10)
 		al.SetIlluminanceCallbackThreshold('>', 200*10, 0);
 
-		System.Console.WriteLine("Press key to exit");
-		System.Console.ReadKey();
+		System.Console.WriteLine("Press enter to exit");
+		System.Console.ReadLine();
 		ipcon.Disconnect();
 	}
 }
