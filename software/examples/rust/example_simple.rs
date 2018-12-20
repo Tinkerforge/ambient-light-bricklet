e4a9bf0ea9a9f48db891ead3405e99862516d72a
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:31:01 +0100

    rust: Reformat examples.

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,6 +1,8 @@
-use std::{error::Error, io};
+use std::{io, error::Error};
 
-use tinkerforge::{ambient_light_bricklet::*, ip_connection::IpConnection};
+use tinkerforge::{ip_connection::IpConnection, 
+                  ambient_light_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -11,11 +13,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let al = AmbientLightBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    // Get current illuminance.
-    let illuminance = al.get_illuminance().recv()?;
-    println!("Illuminance: {} lx", illuminance as f32 / 10.0);
+		// Get current illuminance.
+let illuminance = al.get_illuminance().recv()?;
+		println!("Illuminance: {} lx", illuminance as f32 /10.0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -1,5 +1,8 @@
-use std::{error::Error, io, thread};
-use tinkerforge::{ambient_light_bricklet::*, ip_connection::IpConnection};
+use std::{io, error::Error};
+use std::thread;
+use tinkerforge::{ip_connection::IpConnection, 
+                  ambient_light_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -10,25 +13,25 @@ fn main() -> Result<(), Box<dyn Error>> {
     let al = AmbientLightBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    // Get threshold receivers with a debounce time of 10 seconds (10000ms).
-    al.set_debounce_period(10000);
+		// Get threshold receivers with a debounce time of 10 seconds (10000ms).
+		al.set_debounce_period(10000);
 
-    let illuminance_reached_receiver = al.get_illuminance_reached_callback_receiver();
+     let illuminance_reached_receiver = al.get_illuminance_reached_callback_receiver();
 
-    // Spawn thread to handle received callback messages.
-    // This thread ends when the `al` object
-    // is dropped, so there is no need for manual cleanup.
-    thread::spawn(move || {
-        for illuminance_reached in illuminance_reached_receiver {
-            println!("Illuminance: {} lx", illuminance_reached as f32 / 10.0);
-            println!("Too bright, close the curtains!");
-        }
-    });
+        // Spawn thread to handle received callback messages. 
+        // This thread ends when the `al` object
+        // is dropped, so there is no need for manual cleanup.
+        thread::spawn(move || {
+            for illuminance_reached in illuminance_reached_receiver {           
+                		println!("Illuminance: {} lx", illuminance_reached as f32 /10.0);
+		println!("Too bright, close the curtains!");
+            }
+        });
 
-    // Configure threshold for illuminance "greater than 200 lx".
-    al.set_illuminance_callback_threshold('>', 200 * 10, 0);
+		// Configure threshold for illuminance "greater than 200 lx".
+		al.set_illuminance_callback_threshold('>', 200*10, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
